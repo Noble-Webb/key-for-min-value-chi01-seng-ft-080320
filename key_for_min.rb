bb55da01570bc304d708a25135862a70dd613e01
Justin Webb <webb.n.justin@gmail.com>
2020-07-21 19:45:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,8 @@ def key_for_min_value(name_hash)
   num = 100000
   silly = " "
   name_hash.each do |key, value|
+    if name_hash == []
+      return nil
     if value < num
       
       num = value
